ed377c85ab9da37c0443b4eaa3607a9b10e199c5
1015bean <ibqi1691@gmail.com>
2026-04-16 17:07:39 +0900

    260416 [이론] 데이터바인딩

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// JS코드 작성 부분
+import DataBinding from './components/DataBinding.vue';   //import (불러올 대상) from (불러오는 친구 주소)
+
+</script>
+
+<template>
+ <!-- HTML코드 작성 부분 -->
+   <DataBinding />
+</template>
+
+<style>
+ /* CSS작성부분 */
+</style>
---
+++ src/components/DataBinding.vue
@@ -0,0 +1,68 @@
+<!-- 데이터 바인딩 -->
+<!-- HTML요소와 Vue 인스턴스의 데이터를 동적으로 연결하는 기능(선언적 렌더링) -->
+
+<script setup>
+  import { reactive, ref } from 'vue';   //'vue라이브러리'가 제공하는 툴 'ref'를 불러오기
+
+  // ref( 데이터 )
+      // 데이터 바인딩 객체1 :object
+      // 타입제한 없이 사용할 수 있으나, 주로 프라이미티므 타입(원시타입)에 반응적 참조(유저의 동작에 대한 반응)를 위해 사용
+      // 프라이미티브 타입에 좀 더 나은 성능을 제공
+  const cnt = ref(0);
+      // ref객체의 경우 js에서 접근 시, 해당 값은 ref.value프로퍼티로 접근
+  console.log(cnt.value);
+// --------------------------------------------------------------------------
+  // reactive( 객체 )
+      // 데이터 바인딩 객체2 :object
+      // 객체만 사용 가능하고 , 해당 객체의 반응적 참조를 위해 사용
+  const info = reactive({
+    name: 'kim',
+    age: 20
+  });
+  console.log(info.name);
+// --------------------------------------------------------------------------
+  const color = ref('red');
+
+  function btnToggle() {
+    if(color.value === 'red') {
+      color.value = 'red';
+    }else {
+      color.value = 'blue';
+    }
+  }
+// -----------------------------------------------------------------------------
+  const id = ref('a');
+</script>
+
+<template>
+   <!-- cnt 객체의 "값"에 접근; template에서 객체명 만으로 값에 접근 가능하다 -->
+      <!-- {{ JS데이터(state) }} : state HTML에 불러오는 문법 -->
+      <!-- @이벤트 효과 = '코드' : 이벤트 발생 시 실행할 로직을 연결하는 문법 -->
+   <h1>데이터 바인딩 {{ cnt }}</h1>
+   <button type="button" @click="cnt++">증가</button>
+   <button type="button" @click="cnt--">감소</button>
+
+   <!-- info 객체에 접근 -->
+   <h3>{{ `${info.name} : ${info.age}` }}</h3>
+
+   <!-- 태그 속성명 앞에 ':'을 붙이고, 속성값에 데이터 바인딩 객체를 사용 -->
+   <h3 class="test" :class="color">태그 속성값 데이터 바인딩</h3>
+   <button type="button" @click="btnToggle">색깔 토글</button>
+
+   <!-- 양방향 데이터 바인딩: HTML에서 js데이터에 영향을 줌 -->
+       <!-- v-model 사용 -->
+   <input type="text" v-model="id">
+   <p>{{ id }}</p>
+</template>
+
+<style>
+.red {
+  color: red;
+}
+.blue {
+  color: blue;
+}
+.test {
+  background-color: beige;
+}
+</style>
